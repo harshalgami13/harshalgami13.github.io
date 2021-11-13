2455951e09c742d56ad141e102c5fe35365610d4
Harshal Gami <190540107063@darshan.ac.in>
2021-11-13 12:14:14 +0530

    v1.0.2

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     $('.head-btn').click(function () {
         $('.side-menu').addClass("offcanvas-menu");
+        $('.head-btn').addClass("show");
     })
     $('.close-btn').click(function () {
+        $('.head-btn').removeClass("show");
         $('.side-menu').removeClass("offcanvas-menu");
     })
     $('.head').click(function () {
+        $('.head-btn').removeClass("show");
         $('.side-menu').removeClass("offcanvas-menu");
     })
 
