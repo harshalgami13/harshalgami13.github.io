2fc8b63429d6bf620d03bdf798f17342ec1fbf2c
Harshal Gami <190540107063@darshan.ac.in>
2021-11-13 12:29:31 +0530

    v1.0.2

--- JS/main.js
+++ JS/main.js
@@ -21,4 +21,32 @@ $(document).ready(function () {
     checkwidth();
     // Bind event listener
     $(window).resize(checkwidth);
+
+
+
+
+    // scroll-up button show/hide script
+    $(window).scroll(function () {
+        if (this.scrollY > 150) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+        if (this.scrollY > 50) {
+            $('.social').addClass("show");
+        } else {
+            $('.social').removeClass("show");
+        }
+        if (this.scrollY > 0) {
+            $('.header1').addClass("fixed");
+        } else {
+            $('.header1').removeClass("fixed");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        $('html').css("scrollBehavior", "auto");
+    });
 })
